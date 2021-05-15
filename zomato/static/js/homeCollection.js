d82f9b73d3bf4e10041441e036a5307dc7bd7997
Atharv-777 <63581644+Atharv-777@users.noreply.github.com>
2021-05-15 15:17:09 +0530

    Delete zomato directory

--- zomato/static/js/homeCollection.js
+++
@@ -1,9 +0,0 @@
-                                                                    /*Trending Page JS code*/
-let cardBookmark = document.getElementsByClassName('cardBookmark');
-let cards = document.getElementsByClassName('cards')
-function displayBookmark(num){
-    cardBookmark[num].className = 'cardBookmark open';
-}
-function disappearBoorkmark(num){
-    cardBookmark[num].className = 'cardBookmark';
-}
--- zomato/swiggy/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class SwiggyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'swiggy'
